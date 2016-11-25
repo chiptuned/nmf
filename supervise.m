206cf3bf2d28c031a3e4740d6b81626beff12b3f
Tchikibo <thibaudcognat@gmail.com>
2016-11-25 11:23:17 +0100

    algo pour supervisée
    Initialisation de l'algorithme pour la méthode supervisée avec la mise à
    jour du bruit et du H complet pour 2 instruments

---
+++ supervise.m
@@ -0,0 +1,20 @@
+
+
+
+V = stft_abs;
+W = fixe(:,1:2); % prendre la guitare et le piano par exemple
+Wb = rand(size(V,1), n_dict);
+W = [W,Wb];
+H = rand(n_dict, size(V,2));
+
+for ind = 1:50
+    Hb = H(:,3);
+    Wb = Wb.*((((Wb*Hb).^(beta-2).*V)*Hb')./((Wb*Hb).^(beta-1)*Hb'));
+    H = H .* ((W'*((W*H).^(beta-2).*V))./(W'*(W*H).^(beta-1)));
+    W = [W(:,1:2),Wb];
+
+    handle(1).CData = H;
+    handle(2).CData = W;
+    handle(3).CData = W*H;
+    drawnow;
+end
